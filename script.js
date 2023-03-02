6e158fcadb333934c48f193644f62249212264c7
Arham659 <122161432+Arham659@users.noreply.github.com>
2023-03-02 13:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let yourArray;
 // assign your array the correct value in the next line
 // your code here!
 
-yourArray=[1,56,true,"arham","lovecoding"];
+yourArray=[1,56,true,false,"arham","lovecoding", undefined, null];
 console.log(yourArray);
 // code end
 
